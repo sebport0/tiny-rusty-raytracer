73f75f015db33f715a4c557632098418ff3f62f9
Samuel Bassi <sbassi@eiwa.ag>
2022-01-04 00:35:33 -0300

    [WIP] Write to file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::prelude::*;
+use tiny_rusty_raytracer::Vector3;
+
 fn main() {
-    println!("Hello, world!");
+    render();
+}
+
+fn render() {
+    const WIDTH: usize = 1024;
+    const HEIGHT: usize = 768;
+    let mut framebuffer: Vec<Vector3> = vec![Vector3::new(0.0, 0.0, 0.0); WIDTH * HEIGHT];
+
+    for j in 0..HEIGHT {
+        for i in 0..WIDTH {
+            let index = i + j * WIDTH;
+            framebuffer[index] = Vector3::new((j / HEIGHT) as f64, (i / WIDTH) as f64, 0.0);
+        }
+    }
+
+    let mut ofs = File::create("out.ppm").unwrap();
+    write!(ofs, "P6\n{} {}\n255\n", WIDTH, HEIGHT).unwrap();
 }
